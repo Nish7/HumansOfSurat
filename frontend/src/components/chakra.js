e145afd7882775402a9fb9d48f3f4a2bfcac1623
Nishil <nishilkapadia17@gmail.com>
2021-07-23 12:53:52 +0530

    :bug: ColorModeManager-Chakra

--- frontend/src/components/chakra.js
+++ frontend/src/components/chakra.js
@@ -10,21 +10,25 @@ import {
 
 export function Chakra({ cookies, children }) {
 	// b) Pass `colorModeManager` prop
-	// const colorModeManager =
-	// 	typeof cookies === 'string'
-	// 		? cookieStorageManager(cookies)
-	// 		: localStorageManager;
+	const colorModeManager =
+		typeof cookies === 'string'
+			? cookieStorageManager(cookies)
+			: localStorageManager;
 
-	return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+	return (
+		<ChakraProvider theme={theme} colorModeManager={colorModeManager}>
+			{children}
+		</ChakraProvider>
+	);
 }
 
 // also export a reusable function getServerSideProps
-// export function getServerSideProps({ req }) {
-// 	return {
-// 		props: {
-// 			// first time users will not have any cookies and you may not return
-// 			// undefined here, hence ?? is necessary
-// 			cookies: req.headers.cookie ?? '',
-// 		},
-// 	};
-// }
+export function getServerSideProps({ req }) {
+	return {
+		props: {
+			// first time users will not have any cookies and you may not return
+			// undefined here, hence ?? is necessary
+			cookies: req.headers.cookie ?? '',
+		},
+	};
+}
